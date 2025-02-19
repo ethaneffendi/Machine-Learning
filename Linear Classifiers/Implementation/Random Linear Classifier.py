ff053fa61ea3f937a83d88b0fcbf687fa2731c7e
ethaneffendi <ethaneffendi@outlook.com>
2025-02-18 16:46:15 -0800

    18 February 2025, 4:46 P.

--- Linear Classifiers/Implementation/Random Linear Classifier.py
+++ Linear Classifiers/Implementation/Random Linear Classifier.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-# loss functions
+# loss function
 def zero_one_loss(g, a):
     if g == a:
         return 0
@@ -32,13 +32,13 @@ def training_set_error(theta, theta_not, dataset):
     return sum_of_losses/len(dataset)
 
 # random linear classifier
-def random_linear_classifier(dataset, k):
+def random_linear_classifier(dataset, k, randMinTheta = 0, randMaxTheta = 100, randMinThetaNot = 0, randMaxThetaNot = 100):
     randomParameters = []
     for i in range(k):
-        theta_not = random.randint(0, 100)
-        theta = np.array([])
+        theta_not = random.randint(randMinThetaNot, randMaxThetaNot)
+        theta = np.zeros((dataset[0][0].shape[0], 1))
         for i in range(dataset[0][0].shape[0]):
-            theta = np.append(theta, [random.randint(0,100)])
+            theta[i,0] = random.randint(randMinTheta, randMaxTheta)
         randomParameters.append((theta, theta_not))
     j_star = 0
     for j in range(k):
@@ -46,30 +46,19 @@ def random_linear_classifier(dataset, k):
             j_star = j
     return (randomParameters[j_star][0], randomParameters[j_star][1])
 
-#execution
-positivePoints = [(np.array([[10],[90]]),1),(np.array([[15],[85]]),1),(np.array([[20],[87]]),1),(np.array([[30],[82]]),1),(np.array([[40],[88]]),1),(np.array([[37],[89]]),1),(np.array([[39],[95]]),1),(np.array([[60],[92]]),1),(np.array([[55],[88]]),1),(np.array([[80],[90]]),1),(np.array([[70],[88]]),1),(np.array([[90],[89]]),1)]
-negativePoints = [(np.array([[10],[-90]]),-1),(np.array([[15],[-85]]),-1),(np.array([[20],[-87]]),-1),(np.array([[30],[-82]]),-1),(np.array([[40],[-88]]),-1),(np.array([[37],[-89]]),-1),(np.array([[39],[-95]]),-1),(np.array([[60],[-92]]),-1),(np.array([[55],[-88]]),-1),(np.array([[80],[-90]]),-1),(np.array([[70],[-88]]),-1),(np.array([[90],[-89]]),-1)]
 
-theta, theta_not = random_linear_classifier(positivePoints + negativePoints, 10000000000)
-print(theta)
-print(theta_not)
-print(training_set_error(theta, theta_not, positivePoints + negativePoints))
+# two-dimensional random linear classifier with Matplotlib visualization
+def random_linear_classifier_with_visualization(positivePoints, negativePoints, k, randMinTheta = 0, randMaxTheta = 100, randMinThetaNot = 0, randMaxThetaNot = 100, plotXMin = 0, plotXMax = 100, plotYMin = -100, plotYMax = 100):
+    plt.gca().set_xlim([plotXMin, plotXMax])
+    plt.gca().set_ylim([plotYMin, plotYMax])
+    theta, theta_not = random_linear_classifier(positivePoints + negativePoints, k)
+    plt.scatter(np.array([point[0][0, 0] for point in positivePoints]), np.array([point[0][1, 0] for point in positivePoints]), color = 'blue', marker = "+")
+    plt.scatter(np.array([point[0][0, 0] for point in negativePoints]), np.array([point[0][1, 0] for point in negativePoints]), color = 'red', marker = "_")
+    separatorX = [0, 50, 100]
+    separatorY = [(-theta_not-(theta[0][0]*separatorX[0]))/theta[1][0],(-theta_not-(theta[0][0]*separatorX[1]))/theta[1][0],(-theta_not-(theta[0][0]*separatorX[2]))/theta[1][0]]
+    plt.plot(separatorX, separatorY, color = "black", marker = "o")
+    plt.show()
 
-#visualization
-positiveXCoords = np.array([point[0][0, 0] for point in positivePoints])
-positiveYCoords = np.array([point[0][1, 0] for point in positivePoints])
-negativeXCoords = np.array([point[0][0, 0] for point in negativePoints])
-negativeYCoords = np.array([point[0][1, 0] for point in negativePoints])
-
-plt.gca().set_xlim([0,100])
-plt.gca().set_ylim([-100,100])
-
-plt.scatter(positiveXCoords, positiveYCoords, color = 'blue', marker = "+")
-plt.scatter(negativeXCoords, negativeYCoords, color = 'red', marker = "_")
-
-separatorX = [0, 50, 100]
-separatorY = [(-theta_not-(theta[0]*separatorX[0]))/theta[1],(-theta_not-(theta[0]*separatorX[1]))/theta[1],(-theta_not-(theta[0]*separatorX[2]))/theta[1]]
-
-plt.plot(separatorX, separatorY, color = "black", marker = "o")
-
-plt.show()
+# dataset to try:
+# positivePoints = [(np.array([[10],[90]]),1),(np.array([[15],[85]]),1),(np.array([[20],[87]]),1),(np.array([[30],[82]]),1),(np.array([[40],[88]]),1),(np.array([[37],[89]]),1),(np.array([[39],[95]]),1),(np.array([[60],[92]]),1),(np.array([[55],[88]]),1),(np.array([[80],[90]]),1),(np.array([[70],[88]]),1),(np.array([[90],[89]]),1)]
+# negativePoints = [(np.array([[10],[-90]]),-1),(np.array([[15],[-85]]),-1),(np.array([[20],[-87]]),-1),(np.array([[30],[-82]]),-1),(np.array([[40],[-88]]),-1),(np.array([[37],[-89]]),-1),(np.array([[39],[-95]]),-1),(np.array([[60],[-92]]),-1),(np.array([[55],[-88]]),-1),(np.array([[80],[-90]]),-1),(np.array([[70],[-88]]),-1),(np.array([[90],[-89]]),-1)]
---
+++ Perceptron/Implementation/Perceptron.py
@@ -0,0 +1,81 @@
+import numpy as np
+import random
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# loss function
+def zero_one_loss(g, a):
+    if g == a:
+        return 0
+    else:
+        return 1
+
+def squared_loss(g, a):
+    return (g-a)**2
+
+# sign function
+def sign_function(input):
+    if input > 0:
+        return 1
+    else:
+        return -1
+
+# hypothesis
+def hypothesis(x, theta, theta_not):
+    input = np.dot(theta.T, x) + theta_not
+    return sign_function(input)
+
+# training set error
+def training_set_error(theta, theta_not, dataset):
+    sum_of_losses = 0
+    for i in range(len(dataset)):
+        sum_of_losses += zero_one_loss(hypothesis(dataset[i][0], theta, theta_not), dataset[i][1])
+    return sum_of_losses/len(dataset)
+
+# perceptron
+def perceptron(dataset, T):
+    theta_not = 0
+    theta = np.zeros((dataset[0][0].shape[0], 1))
+    for j in range(T):
+        for i in range(len(dataset)):
+            if(dataset[i][1] * (np.dot(theta.T, dataset[i][0])+theta_not) <= 0):
+                theta = np.add(theta, dataset[i][1] * dataset[i][0])
+                theta_not = theta_not + dataset[i][1]
+    return (theta, theta_not)
+
+# two-dimensional perceptron with Matplotlib visualization
+def perceptron_with_visualization(positivePoints, negativePoints, T, plotXMin = 0, plotXMax = 100, plotYMin = -100, plotYMax = 100):
+    dataset = positivePoints + negativePoints
+    theta_not = 0
+    theta = np.zeros((dataset[0][0].shape[0], 1))
+    plt.ion()
+    fig, ax = plt.subplots()
+    plt.gca().set_xlim([plotXMin, plotXMax])
+    plt.gca().set_ylim([plotYMin, plotYMax])
+    ax.scatter(np.array([point[0][0, 0] for point in positivePoints]), np.array([point[0][1, 0] for point in positivePoints]), color = 'blue', marker = "+")
+    ax.scatter(np.array([point[0][0, 0] for point in negativePoints]), np.array([point[0][1, 0] for point in negativePoints]), color = 'red', marker = "_")
+    lineXCoords = [plotXMin, (plotXMin+plotXMax)/2, plotXMax]
+    lineYCoords = [1,1,1]
+    ln, = ax.plot(lineXCoords, lineYCoords)
+    for j in range(T):
+        for i in range(len(dataset)):
+            if(dataset[i][1] * (np.dot(theta.T, dataset[i][0])+theta_not) <= 0):
+                theta = np.add(theta, dataset[i][1] * dataset[i][0])
+                theta_not = theta_not + dataset[i][1]
+                ax.cla()
+                ax.scatter(np.array([point[0][0, 0] for point in positivePoints]), np.array([point[0][1, 0] for point in positivePoints]), color = 'blue', marker = "+")
+                ax.scatter(np.array([point[0][0, 0] for point in negativePoints]), np.array([point[0][1, 0] for point in negativePoints]), color = 'red', marker = "_")
+                ax.plot(lineXCoords, [(-theta_not-(theta[0][0]*lineXCoords[0]))/theta[1][0],(-theta_not-(theta[0][0]*lineXCoords[1]))/theta[1][0],(-theta_not-(theta[0][0]*lineXCoords[2]))/theta[1][0]])
+                plt.gca().set_xlim([plotXMin, plotXMax])
+                plt.gca().set_ylim([plotYMin, plotYMax])
+                plt.draw()
+                plt.pause(2)
+    plt.show()
+
+
+
+
+
+# dataset to try
+# positivePoints = [(np.array([[-2],[3]]),1), (np.array([[0],[1]]),1), (np.array([[2],[-1]]),1)]
+# negativePoints = [(np.array([[-2],[1]]),-1), (np.array([[0],[-1]]),-1), (np.array([[2],[-3]]),-1)]
