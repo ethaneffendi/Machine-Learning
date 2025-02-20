27c624a222f5d9ca424ea701dd5ebd4eeac3f1d7
ethaneffendi <ethaneffendi@outlook.com>
2025-02-20 14:23:56 -0800

    20 February 2025, 2:23 P.M.

--- (7) Regression/Implementation/Analytical Ridge Regression.py
+++ (7) Regression/Implementation/Analytical Ridge Regression.py
@@ -1,2 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+# analytical ridge regression
+def analytical_ridge_regression(dataset, lam):
+    n = len(dataset[0])
+    W = np.zeros((len(dataset),n+1))
+    T = np.zeros((len(dataset), 1))
+    I = np.identity(W.shape[0])
+    for i in range(len(dataset)):
+        T[i][0] = dataset[i][1]
+        for j in range(n):
+            W[i][j] = dataset[i][0][j]
+        W[i][n] = 1
+    return np.linalg.inv(((W.T * W) + (n * lam * I))) * (W.T * T)
+
+positivePoints = [(np.array([[-2],[3]]),1), (np.array([[0],[1]]),1), (np.array([[2],[-1]]),1)]
+print(analytical_ridge_regression(positivePoints, 1))
